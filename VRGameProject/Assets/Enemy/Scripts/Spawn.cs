7b957654aec08525109656d29debbbf10a5930ee
Yodravee <yodravee.kuso@bumail.net>
2022-08-04 01:30:21 +0700

    editSpawnScripts

--- VRGameProject/Assets/Enemy/Scripts/Spawn.cs
+++ VRGameProject/Assets/Enemy/Scripts/Spawn.cs
@@ -4,14 +4,38 @@ namespace Enemy
 {
     public class Spawn : MonoBehaviour
     {
+        [Header("Wave1")] [SerializeField] private float start1;
+        [SerializeField] float end1;
+        [Header("Wave2")] [SerializeField] private float start2;
+        [SerializeField] float end2;
+        [Header("Wave3")] [SerializeField] private float start3;
+        [SerializeField] float end3;
+        [Header("Wave4")] [SerializeField] private float start4;
+        [SerializeField] float end4;
+        
+        [Header("numberOfEnemyWave1")]
+        [SerializeField]
+        int num1;
+        [Header("numberOfEnemyWave2")]
+        [SerializeField]
+        int num2; 
+        [Header("numberOfEnemyWave3")]
+        [SerializeField]
+        int num3;
+        [Header("numberOfEnemyWave4")]
+        [SerializeField]
+        int num4;
+        
+        
+        [Header("enterGameobject")]
         [SerializeField] private GameObject  enemy1,enemy2,enemy3;
         [SerializeField] private Transform   spawn1,spawn2,spawn3;
         private GameObject[] enemyS;
         private Transform[]  spawnS;
         public float timecount;
-        public float spawnRate;
+        public float spawnRate=1;
         int numEnemies=0;
-     
+      
         
 
         public void Start()
@@ -25,27 +49,26 @@ namespace Enemy
         public void FixedUpdate()
         {
             timecount += Time.deltaTime;
-         
-            
             Debug.Log($"{(int)timecount}Secon");
-            
-           
-       
-        
-            if (timecount>=5 && timecount<=39)
+            if (timecount>=start1 && timecount<=end1)
             {
                 spawnRate += Time.deltaTime;
                 SpawnPoint();
             }
-            if (timecount>=40&& timecount<=119)
+            if (timecount>=start2&& timecount<=end2)
             {
                 spawnRate += Time.deltaTime;
                 SpawnPoint1();
             } 
-            if (timecount>=120 && timecount<=200)
+            if (timecount>=start3 && timecount<=end3)
             {
                 spawnRate += Time.deltaTime;
                 SpawnPoint2();
+            }
+            if (timecount>=start4 && timecount<=end4)
+            {
+                spawnRate += Time.deltaTime;
+                SpawnPoint3();
             }
         
         }
@@ -61,7 +84,7 @@ namespace Enemy
 
         void SpawnPoint()
         {
-            if ( spawnRate >= 1f && numEnemies<2)
+            if ( spawnRate >= 1f && numEnemies<num1)
             {
                 spawnRate = 0;
                 SpawnEnemy();
@@ -71,7 +94,7 @@ namespace Enemy
         }
         void SpawnPoint1()
         {
-            if ( spawnRate >= 1f && numEnemies<30)
+            if ( spawnRate >= 1f && numEnemies<num2)
             {
                 spawnRate = 0;
                 SpawnEnemy();
@@ -81,7 +104,17 @@ namespace Enemy
         }
         void SpawnPoint2()
         {
-            if ( spawnRate >= 1f && numEnemies<40)
+            if ( spawnRate >= 1f && numEnemies<num3)
+            {
+                spawnRate = 0;
+                SpawnEnemy();
+         
+            }
+        
+        }
+        void SpawnPoint3()
+        {
+            if ( spawnRate >= 1f && numEnemies<num4)
             {
                 spawnRate = 0;
                 SpawnEnemy();
